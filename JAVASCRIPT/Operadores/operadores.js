da79eff5660c192987dfb58cb6c3b9a5aa52d0e8
FredyMarG <fredymartinezquinto@hotmail.com>
2026-03-16 11:49:37 -0500

    operadores 2

--- JAVASCRIPT/Operadores/operadores.js
+++ JAVASCRIPT/Operadores/operadores.js
@@ -22,6 +22,18 @@ console.log(numero);
 //OPERADORES DE COMPARACION
 numero = 23
 let numero2 = 13 // = Asignación
+console.log(numero != numero2);
 console.log(numero == numero2);
 console.log(numero >= numero2);
 console.log(numero2 < numero);
+
+//OPERADORES LOGICOS
+let num1 = 12
+let num2 = 24
+af1 = num1 < num2
+af2 = num1 != num2
+//AND
+console.log(af1 && af2);
+//OR
+af1 = num1 > num2
+console.log(af1 || af2);
