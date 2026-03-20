aab9801b7c41eb0886ca6e0060d2f3ea28f39841
FredyMarG <fredymartinezquinto@hotmail.com>
2026-03-20 14:29:43 -0500

    Ejercicio 3 completo

--- JAVASCRIPT/EJERCICIOS/Ejercicio3/main.js
+++ JAVASCRIPT/EJERCICIOS/Ejercicio3/main.js
@@ -1,43 +1,52 @@
-let cantidad = parseInt(prompt("¿Cuantos alumnos son?")),
-    alumnosTotales = [],
-    alumnos = document.getElementById("cantidad"),
-    data = document.getElementById("data")
-    if(isNaN(cantidad) || cantidad == undefined){
-        cantidad = 0
+let sol = document.getElementById("sol"),
+    main_sol = document.getElementById("main_sol")
+
+function result() {
+    let cantidad = parseInt(prompt("¿Cuantos alumnos son?")),
+        alumnosTotales = [],
+        alumnos = document.getElementById("cantidad"),
+        data = document.getElementById("data");
+    if (isNaN(cantidad) || cantidad == undefined) {
+        cantidad = 0;
     }
-    alumnos.innerHTML = `El total de alumnos es ${cantidad}`
+    alumnos.innerHTML = `El total de alumnos es ${cantidad}`;
 
-for (let i = 0; i < cantidad; i++) {
-    alumnosTotales[i] = [prompt("Nombre del alumno "+ (i+1)), 0]
-}
-
-const asistencia = (nombre,p)=>{
-    let presencia = prompt(`Asistencia del alumno ${nombre}`) //Almacena si es presente o ausente dependiendo el nombre del alumno
-    if (presencia == "p" || presencia == "P") {
-        alumnosTotales[p][1]++ //estoy tomando la posicion del primer array [p] y estoy tomando la posicion 1[1] dentro del segundo array
+    for (let i = 0; i < cantidad; i++) {
+        alumnosTotales[i] = [prompt("Nombre del alumno " + (i + 1)), 0];
     }
-}
 
-for (let i = 0; i < 30; i++) {
+    const asistencia = (nombre, p) => {
+        let presencia = prompt(`Asistencia del alumno ${nombre}`); //Almacena si es presente o ausente dependiendo el nombre del alumno
+        if (presencia == "p" || presencia == "P") {
+            alumnosTotales[p][1]++; //estoy tomando la posicion del primer array [p] y estoy tomando la posicion 1[1] dentro del segundo array
+        }
+    };
+
+    for (let i = 0; i < 30; i++) {
+        for (alumno in alumnosTotales) {
+            asistencia(alumnosTotales[alumno][0], alumno);
+        }
+    }
+
     for (alumno in alumnosTotales) {
-        asistencia(alumnosTotales[alumno][0], alumno)
+        if (30 - alumnosTotales[alumno][1] > 18) {
+            data.innerHTML += `<tr style="color: red;">
+            <td>${+alumno + 1}</td>
+            <td>${alumnosTotales[alumno][0]/**nombre */}</td>
+            <td>${alumnosTotales[alumno][1]/**cantidad de asistencias */}</td>
+            <td>${30 - alumnosTotales[alumno][1] + "REPROBADO"/**cantidad de fallas */}</td>
+        </tr>`;
+        } else {
+            data.innerHTML += `<tr>
+            <td>${+alumno + 1}</td>
+            <td>${alumnosTotales[alumno][0]}</td>
+            <td>${alumnosTotales[alumno][1]}</td>
+            <td>${30 - alumnosTotales[alumno][1]}</td>
+        </tr>`;
+        }        
     }
 }
 
-for (alumno in alumnosTotales) {
-    if (30 - alumnosTotales[alumno][1] > 18) {
-        data.innerHTML += `<tr style="color: red;">
-            <td>${alumno + 1}</td>
-            <td>${alumnosTotales[alumno][0]}</td>
-            <td>${alumnosTotales[alumno][1]}</td>
-            <td>${30 - alumnosTotales[alumno][1]}</td>
-        </tr>`;
-    } else {
-        data.innerHTML += `<tr>
-            <td>${alumno + 1}</td>
-            <td>${alumnosTotales[alumno][0]}</td>
-            <td>${alumnosTotales[alumno][1]}</td>
-            <td>${30 - alumnosTotales[alumno][1]}</td>
-        </tr>`;
-    }
-}
+sol.addEventListener("click", ()=>{
+    main_sol.style.display = "block"
+})
