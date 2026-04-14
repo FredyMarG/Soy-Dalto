7c1b7bc8ddcb8b2c0426e9d7ff7b58a8637e40a5
FredyMarG <fredymartinezquinto@hotmail.com>
2026-04-13 23:27:46 -0500

    calculadora avanzada

--- JAVASCRIPT/EJERCICIOS/7 Calculadora Avanzada/main.js
+++ JAVASCRIPT/EJERCICIOS/7 Calculadora Avanzada/main.js
@@ -1,67 +1,87 @@
-let mobile = document.getElementById("mobile"),
-    sol = document.getElementById("main-sol")
-class App {
-    constructor(descargas, puntuacion, peso) {
-        this.descargas = descargas;
-        this.puntuacion = puntuacion;
-        this.peso = peso;
-        this.iniciada = false;
-        this.instalada = false;
+let respuesta = document.getElementById("respuesta"),
+    calc = document.getElementById("calcular"),
+    sol = document.getElementById("main-sol"),
+    num1,
+    num2;
+
+class Calculadora{
+    constructor(){
     }
-    instalar(){
-        if (this.instalada === false) {
-            this.instalada = true;
-            return "App Instalada Correctamente";
-        }
+    sumar(num1,num2){
+        return parseInt(num1) + parseInt(num2)
     }
-    desinstalar(){
-        if (this.instalada === true) {
-            this.instalada = false;
-            return "App Desinstalada Correctamente";
-        }
+    restar(num1,num2){
+        return parseInt(num1) - parseInt(num2)
     }
-    abrir(){
-        if (this.iniciada === false && this.instalada === true) {
-            this.iniciada = true;
-            return "App Iniciada";
-        }   
+    multiplicar(num1,num2){
+        return parseInt(num1) * parseInt(num2)
     }
-    cerrar(){
-        if (this.iniciada === true && this.instalada === true) {
-            this.iniciada = false;
-            return "App Cerrada";
-        }
+    dividir(num1,num2){
+        return parseInt(num1) / parseInt(num2)
     }
-    appInfo(){
-        return `
-        Descargas: <b>${this.descargas}</b><br>
-        Puntuación: <b>${this.puntuacion}</b><br>
-        Peso: <b>${this.peso}</b><br>
-        `;
+
+    potencia(num,exp){
+        return parseInt(Math.pow(num,exp))
+    }
+
+    raizCuadrada(num){
+        return parseInt(Math.sqrt(num))
+    }
+
+    raizCubica(num){
+        return parseInt(Math.cbrt(num))
     }
 }
 
-function mostrar(){
-    let app = [
-        new App("1.000", "5 estrellas", "900mb"),
-        new App("5.000", "4 estrellas", "400mb"),
-        new App("10000", "3 estrellas", "100mb"),
-        new App("23.000", "4.5 estrellas", "1GB"),
-        new App("900", "2.9 estrella", "250mb"),
-        new App("17", "4.8 estrellas", "522mb"),
-        new App("42.856", "3.7 estrellas", "723mb")]
+const calculadora = new Calculadora();
 
-        for (let i = 0; i < app.length; i++) {
-            mobile.innerHTML += `<tr>
-                            <td>${app[i].descargas}</td>
-                            <td>${app[i].puntuacion}</td>
-                            <td>${app[i].peso}</td>
-                            <td>${app[i].instalar()}</td>
-                            <td>${app[i].abrir()}</td>
-                            <td>${app[i].cerrar()}</td>
-                            <td>${app[i].desinstalar()}</td>
-                        </tr>`;
+function calculo() {
+    let operacion = document.getElementById("operacion")
+    operacion = operacion.value
+    if (operacion == 1) {
+        let numero1 = prompt("Primer número para sumar")
+        let numero2 = prompt("Segundo número para sumar")
+        resultado = calculadora.sumar(numero1,numero2)
+        respuesta.innerHTML = `${numero1} + ${numero2} es ${resultado}`
+    }else if (operacion == 2) {
+        let numero1 = prompt("Primer número para restar")
+        let numero2 = prompt("Segundo número para restar")
+        resultado = calculadora.restar(numero1,numero2)
+        respuesta.innerHTML = `${numero1} - ${numero2} es ${resultado}`
+    }else if (operacion == 3) {
+        let numero1 = prompt("Primer número para múltipicar")
+        let numero2 = prompt("Segundo número para múltipicar")
+        resultado = calculadora.multiplicar(numero1,numero2)
+        respuesta.innerHTML = `${numero1} * ${numero2} es ${resultado}`
+    }else if (operacion == 4) {
+        let numero1 = prompt("Primer número para dividir")
+        let numero2 = prompt("Segundo número para dividir")
+        if (numero2 == 0) {
+            respuesta.innerHTML = `!Error¡ no se puede dividir entre 0`
+        }else{
+            resultado = calculadora.dividir(numero1,numero2)
+            respuesta.innerHTML = `${numero1} / ${numero2} es ${resultado}`
         }
+    }
+    else if (operacion == 5) {
+        let numero1 = prompt("Número para calcular la potencia")
+        let numero2 = prompt("Ingrese el exponente para calcular la potencia")
+        resultado = calculadora.potencia(numero1,numero2)
+        respuesta.innerHTML = `${numero1} a la ${numero2} es ${resultado}`
+    }
+    else if (operacion == 6) {
+        let numero1 = prompt("Número para calcular la raíz cuadrada")
+        resultado = calculadora.raizCuadrada(numero1)
+        respuesta.innerHTML = `La raíz cuadrada de ${numero1} es ${resultado}`
+    }
+    else if (operacion == 7) {
+        let numero1 = prompt("Número para calcular la raíz cúbica")
+        resultado = calculadora.raizCubica(numero1)
+        respuesta.innerHTML = `La raíz cúbica de ${numero1} es ${resultado}`
+    }
+    else{
+            respuesta.innerHTML = "El valor ingresado no es correcto"
+    }
 
-    sol.style.display = "block";
+    sol.style.display = "block"
 }
